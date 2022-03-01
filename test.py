41693ee5b6e9829edaaaaaf8c183200e0eccb5e0
SanjayEngineer <33143882+sanjayengineer121@users.noreply.github.com>
2022-03-01 16:59:54 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,267 @@
+from flask import Flask, render_template,json,request
+from enum import Enum
+from flask_sqlalchemy import SQLAlchemy
+import os
+import flask
+import webbrowser
+import logging
+import requests
+import datetime
+now = datetime.datetime.now()
+
+application = Flask(__name__)
+application.config['SQLALCHEMY_DATABASE_URI']='sqlite:///sales.db'
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db=SQLAlchemy(application)
+
+
+
+
+
+
+
+
+
+
+@application.route("/tallymsg")
+def demo_world():
+    mobile = request.args.get('mobile')
+    message = request.args.get('message')
+    attach = request.args.get('attach')
+    print(message)
+    print(mobile)
+    webbrowser.open(attach)
+    
+    target = "Rs."
+    words = message.split()
+
+    for i,w in enumerate(words):
+        if w == target:
+            prch=(words[i+1])
+            print('=======================|PRICE|===============================')
+            print("RS",prch)
+    target = 'Sir,'
+    for i,w in enumerate(words):
+        if w == target:
+            name=(words[i+1])
+            print("==============================|NAME|=============================")
+            print(name)
+
+    target = 'No'
+    for i,w in enumerate(words):
+        if w == target:
+            vch=(words[i+1])
+            print('===============================|VOUCHER NO.===============================')
+            print(vch)
+
+    target = 'Dated' or "on"
+    for i,w in enumerate(words):
+        if w == target:
+            dat=(words[i+2])
+            print("======================================|DATE|================================")
+            d1=words[i+2]
+            d2=words[i+1]
+    
+            file = open('geek.txt','a')
+            file.write("\n\n===============================================================================\n\n")
+            file.write("======================================|mobile|=================================\n")
+            file.write(mobile)
+            file.write("\n====================================|SALES MSG|================================\n")
+            file.write(message)
+            file.write("\n======================================|PURCHASE|===============================\n")
+            file.write(prch)
+            file.write("\n===================================|VOUCHER NO|================================\n")
+            file.write(vch)
+            file.write("\n======================================|DATE|===================================\n")
+            file.write(d1)
+            file.write("\n======================================|NAME|====================================\n")
+            file.write(name)
+            file.write("\n\n===============================================================================\n\n")
+            file.close()
+
+            file = open('current.txt','w')
+            file.write("\n\n===============================================================================\n\n")
+            file.write("======================================|mobile|=================================\n")
+            file.write(mobile)
+            file.write("\n====================================|SALES MSG|================================\n")
+            file.write(message)
+            file.write("\n======================================|PURCHASE|===============================\n")
+            file.write(prch)
+            file.write("\n===================================|VOUCHER NO|================================\n")
+            file.write(vch)
+            file.write("\n======================================|DATE|===================================\n")
+            file.write(d1)
+            file.write("\n======================================|NAME|====================================\n")
+            file.write(name)
+            file.write("\n\n===============================================================================\n\n")
+            file.close()
+       
+
+    return "HELLO"
+
+@application.route("/margsms")
+def demo_world1():
+    mobile = request.args.get('mobile')
+    message = request.args.get('message')
+    attach = request.args.get('attach')
+    print('============================================================\n')
+    print(mobile)
+    print('============================================================\n')
+    print(message)
+    str=message.split()
+    
+    print(str)
+   
+    
+    target = 'Patient'
+    for i,w in enumerate(str):
+        if w == target:
+            cust=(str[i+1])
+            print('===============================|NAME.===============================')
+            print(cust)
+
+    target = 'Bill'
+    for i,w in enumerate(str):
+        if w == target:
+            vch=(str[i+1])
+            print('===============================|VOUCHER NO.===============================')
+            print(vch)
+
+    target = "Bill"
+    for i,w in enumerate(str):
+        if w == target:
+            dat=(str[i+2])
+            print("======================================|DATE|================================")
+            print(dat)
+    target='Patient'
+    for i,w in enumerate(str):
+        if w == target:
+            amo=(str[i-1])
+            print("==============================|Amount|=============================")
+            print(amo)
+
+    print('=======================|Payment Mode|===============================')
+    print(str[0])
+
+    file = open('current.txt','w')
+    file.write("------------------------Marg Bills--------------------------------------------------------")
+    file.write("\n\n===============================================================================\n\n")
+    file.write("======================================|mobile|=================================\n")
+    file.write(mobile)
+    file.write("\n====================================|SALES MSG|================================\n")
+    file.write(message)
+    file.write("\n======================================|SALES AMOUNT|===============================\n")
+    file.write(amo)
+    file.write("\n===================================|VOUCHER NO|================================\n")
+    file.write(vch)
+    file.write("\n======================================|DATE|===================================\n")
+    file.write(dat)
+    file.write("\n======================================|NAME|====================================\n")
+    file.write(cust)
+    file.write("\n======================================|Mode Of Payment|====================================\n")
+    file.write(str[0])
+    file.write("\n\n===============================================================================\n\n")
+    file.close()
+    file = open('geek.txt','a')
+    file.write("------------------------Marg Bills--------------------------------------------------------")
+    file.write("\n\n===============================================================================\n\n")
+    file.write("======================================|mobile|=================================\n")
+    file.write(mobile)
+    file.write("\n====================================|SALES MSG|================================\n")
+    file.write(message)
+    file.write("\n======================================|SALES AMOUNT|===============================\n")
+    file.write(amo)
+    file.write("\n===================================|VOUCHER NO|================================\n")
+    file.write(vch)
+    file.write("\n======================================|DATE|===================================\n")
+    file.write(dat)
+    file.write("\n======================================|NAME|====================================\n")
+    file.write(cust)
+    file.write("\n======================================|Mode Of Payment|====================================\n")
+    file.write(str[0])
+    file.write("\n\n===============================================================================\n\n")
+    file.close()
+
+    return "HELLO"
+
+@application.route("/busysms")
+def demo_world2():
+    mobile = request.args.get('mobile')
+    message = request.args.get('message')
+    print('============================================================\n')
+    print(mobile)
+    print('============================================================\n')
+    print(message)
+    str=message.split()
+    print(str)
+
+    target = 'Dear'
+    for i,w in enumerate(str):
+        if w == target:
+            cust=(str[i+1])
+            print('===============================|NAME.===============================')
+            print(cust)
+
+    target = 'attached'
+    for i,w in enumerate(str):
+        if w == target:
+            vch=(str[i+1])
+            print('===============================|VOUCHER TYPE.===============================')
+            print(vch)
+
+    target='Rs'
+    for i,w in enumerate(str):
+        if w == target:
+            amo=(str[i+1])
+            print("==============================|Amount|=============================")
+            print(amo)
+
+    file = open('current.txt','w')
+    file.write("------------------------Busy Bills--------------------------------------------------------")
+    file.write("\n\n===============================================================================\n\n")
+    file.write("======================================|mobile|=================================\n")
+    file.write(mobile)
+    file.write("\n====================================|SALES MSG|================================\n")
+    file.write(message)
+    file.write("\n======================================|SALES AMOUNT|===============================\n")
+    file.write(amo)
+    file.write("\n===================================|VOUCHER TYPE|================================\n")
+    file.write(vch)
+    file.write("\n======================================|DATE|===================================\n")
+    file.write(str(now))
+    file.write("\n======================================|NAME|====================================\n")
+    file.write(cust)
+    file.write("\n\n===============================================================================\n\n")
+    file.close()
+    file = open('geek.txt','a')
+    file.write("------------------------Marg Bills--------------------------------------------------------")
+    file.write("\n\n===============================================================================\n\n")
+    file.write("======================================|mobile|=================================\n")
+    file.write(mobile)
+    file.write("\n====================================|SALES MSG|================================\n")
+    file.write(message)
+    file.write("\n======================================|SALES AMOUNT|===============================\n")
+    file.write(amo)
+    file.write("\n===================================|VOUCHER NO|================================\n")
+    file.write(vch)
+    file.write("\n======================================|DATE|===================================\n")
+    file.write(str(now))
+    file.write("\n======================================|NAME|====================================\n")
+    file.write(cust)
+    file.write("\n\n===============================================================================\n\n")
+    file.close()
+
+@application.route("/", methods = ['GET', 'POST'])
+def home():
+    return flask.send_file("current.txt")
+
+@application.route('/sales', methods = ['GET', 'POST'])
+def uploader():
+    return flask.send_file("geek.txt")
+
+
+ 
+
+if __name__=="__main__":
+    application.run(debug=True,port=8000)
+    
